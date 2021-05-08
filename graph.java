153b08b9a923ba2b05a0a2f6838bac718b7f9079
himanshugsn30 <himanshugsn30@gmail.com>
2021-05-08 11:30:26 +0530

    first commit

---
+++ graph.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class graph {
+    static ArrayList<ArrayList<Integer>> arr = new ArrayList<ArrayList<Integer>>();
+    int[] visted;
+    public static void dfs(int v){
+        visited[v] = 1;
+        System.out.println(v);
+        for(int child : arr.get(v)){
+            if(visited[child] == 0){
+                dfs(child);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int m = s.nextInt();
+        // Adjacency list implementation of graph
+        
+        visited = new int[n];
+
+        while(m > 0){
+            int a = s.nextInt();
+            int b = s.nextInt();
+            arr.get(a).add(b);
+            m--;
+        }
+    }
+}
